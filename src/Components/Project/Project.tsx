321b235132ba969e433b18adba11914ed9186771
Dustin-Har <harbaughdustin@yahoo.com>
2022-04-10 13:34:26 -0400

    Add style to editable text

--- src/Components/Project/Project.tsx
+++ src/Components/Project/Project.tsx
@@ -3,6 +3,7 @@ import { ProjectTodos } from '../../Utilities/interface'
 import EditableText from '../EditableText/EditableText'
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import Todo from '../Todo/Todo';
+import './Project.css'
 
 export default function Project({ id, name, projectTodos, updateProjectName, removeProject, addTodoToProject, updateCompletedTodo, removeTodo }: { id: number, name: string, projectTodos: ProjectTodos[] | [], updateProjectName: (id: number, name: string) => void, removeProject: (id: number) => void, addTodoToProject: (id: number, todo: string) => void, updateCompletedTodo: (projectId: number, todoId: number) => void, removeTodo: (projectId: number, todoId: number) => void }) {
 
@@ -55,11 +56,11 @@ export default function Project({ id, name, projectTodos, updateProjectName, rem
 	}
 
 	return (
-		<div>
+		<div className='project'>
 			<EditableText text={name} updateName={updateName}/>
-			<button onClick={event => deleteProject(event)}><DeleteOutlineOutlinedIcon /></button>
+			<button onClick={event => deleteProject(event)} className='delete-proj'><DeleteOutlineOutlinedIcon /></button>
 			{todoCards}
-			<form name='todoForm' onSubmit={addTodo}>
+			<form name='todoForm' className='todo-form' onSubmit={addTodo}>
 				<input type="text" placeholder='Add a todo' value={todoFormInput} onChange={updateTodoFormInput} />
 				<button type='submit' disabled={!todoFormInput.trim()}>Add Todo</button>
 			</form>
